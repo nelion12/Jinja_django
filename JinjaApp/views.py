2a2fba3271a9c87f25f8ded91ac44c4f47ecc29b
nelion12 <eliudnelion12@gmail.com>
2024-11-08 12:16:43 +0300

    initial commit

---
+++ JinjaApp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def about(request):
+    return render(request, 'about.html')
+def base(request):
+    return render(request, 'base.html')
+def contact(request):
+    return render(request, 'contact.html')
+def home(request):
+    return render(request, 'home.html')
+def services(request):
+    return render(request, 'services.html')
